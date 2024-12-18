b925b31f0839d6b49cd5c4851e726d6cb3965926
danny-reilly <daniel_reilly@students.nksd.net>
2024-12-17 19:56:46 -0500

    Update PlanB.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanB.java
@@ -11,12 +11,14 @@ public class PlanB extends ScrimmageAuto {
 
         //1.6s ≈ 2ft at 0.25speed
         //ROTATION: 6.5S ≈ 360 °
-        //basket specimen
+        //basket specimen 1
         linearSlide.setPower(0.5f);
         driveSeconds(3, 0.25f, dir.BACKWARD);
         driveSeconds(1, 0.25f, dir.LEFTROT);
         moveArm(0);
         sleep(3000);
+
+        //get spec 2
         moveArm(0.875);
         sleep(2000);
         driveSeconds(1, 0.25f, dir.FORWARD);
@@ -29,16 +31,34 @@ public class PlanB extends ScrimmageAuto {
         sleep(1000);
         driveSeconds(0.5, 0.25f, dir.FORWARD);
         linearSlide.setPower(0);
-        hClawServo.setPosition(0.375);
-        sleep(1000);
+        //hClawServo.setPosition(0.375);
+        //sleep(1000);
         hClawServo.setPosition(0.75);
         sleep(1000);
-        transferSample();
-        while(true) {
-            telemetry.addData("step:", transferStep);
-            telemetry.addData("timer:", transferTimer);
-            telemetry.update();
-        }
 
+        //transfer spec 2
+            hArmOpen.setPosition(0.15);
+            hLinearSlide.setPosition(0.605);
+            sleep(1200);
+            hClawServo.setPosition(0.375);
+            sleep(500);
+            hLinearSlide.setPosition(0.575);
+            sleep(200);
+            hClawServo.setPosition(0.75);
+            sleep(100);
+
+        //basket spec 2
+        hLinearSlide.setPosition(0.475);
+        sleep(100);
+        driveSeconds(0.5, 0.25f, dir.BACKWARD);
+        linearSlide.setPower(0.5f);
+        driveSeconds(1, 0.25f, dir.RIGHTROT);
+        driveSeconds(1, 0.25f, dir.BACKWARD);
+        moveArm(0);
+        sleep(3000);
+        moveArm(0.875);
+        driveSeconds(0.5, 0.25f, dir.FORWARD);
+        linearSlide.setPower(0);
+        
     }
 }
