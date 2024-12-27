985b5a128dac012a12db4d6ab456cb2151dd4273
Dakspieth <jrb.games09@gmail.com>
2024-12-27 10:50:31 -0500

    Update LeftSideAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftSideAuto.java
@@ -12,7 +12,7 @@ public class LeftSideAuto extends ScrimmageAuto {
         //1.6s ≈ 2ft at 0.25speed
         //ROTATION: 6.5S ≈ 360 °
         //basket specimen 1
-        hClawServo.setPosition(0.75);
+      /*  hClawServo.setPosition(0.75);
         driveSeconds(0.05f, 0.25f, dir.LEFT);
         linearSlide.setPower(0.5f);
         driveSeconds(3.5f, 0.25f, dir.BACKWARD);
@@ -68,20 +68,20 @@ public class LeftSideAuto extends ScrimmageAuto {
 
 
 
-/*
+*/
 /////////////////////////////////////////
-WORKING AUTO(OLD)
+//WORKING AUTO(OLD)
 /////////////////////////////////////////
 driveSeconds(0.2, 0.25f, dir.LEFT);
 linearSlide.setPower(0.5f);
 driveSeconds(3, 0.25f, dir.BACKWARD);
 driveSeconds(1, 0.25f, dir.LEFTROT);
-moveArm(0);
+vArmServo.setPosition(0);
 hArmOpen.setPosition(0.15);
 hClawServo.setPosition(0.377);
 sleep(1000);
 //get spec 2
- moveArm(0.875);
+ vArmServo.setPosition(0.875);
 sleep(1000);
 driveSeconds(1, 0.25f, dir.FORWARD);
 driveSeconds(1, 0.25f, dir.LEFTROT);
@@ -99,8 +99,7 @@ sleep(1000);
     hClawServo.setPosition(0.6);
     sleep(500);
            hLinearSlide.setPosition(0.7);
-    sleep(200);
-    sleep(100);
+    sleep(300);
     hArmOpen.setPosition(0.15);
     sleep(400);
        //basket spec 2
@@ -111,9 +110,9 @@ sleep(1000);
        driveSeconds(0.5, 0.25f, dir.FORWARD);
        driveSeconds(1, 0.25f, dir.RIGHTROT);
        driveSeconds(1, 0.25f, dir.BACKWARD);
-       moveArm(0);
+       vArmServo.setPosition(0);
        sleep(1000);
-       moveArm(0.875);
+       vArmServo.setPosition(0.875);
        driveSeconds(0.5, 0.25f, dir.FORWARD);
        sleep(1000);
        linearSlide.setPower(0);
@@ -121,9 +120,9 @@ sleep(1000);
 }
 
 /////////////////////////////////////////
-WORKING AUTO(OLD)
+//WORKING AUTO(OLD)
 /////////////////////////////////////////
- */
+
 
 
 
