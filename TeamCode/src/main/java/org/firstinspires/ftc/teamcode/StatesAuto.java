e8f9e5d6386ff37acc9fe4473d545b4a2a4902ef
dannyr <daniel_reilly@students.nksd.net>
2025-01-06 18:40:38 -0500

    autoStuffffff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StatesAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StatesAuto.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,17 +10,21 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class StatesAuto extends LinearOpMode {
 
     ///////////////////////////////code///////////////////////////////
-    private ElapsedTime runtime = new ElapsedTime();
+    final private ElapsedTime runtime = new ElapsedTime();
     //constants for inch functions
     static final double ticksPerRev = 1440;
     static final double wheelDiameter = 3.5;     // For figuring circumference (in inches)
     static final double ticksPerInch  = ticksPerRev / (wheelDiameter * Math.PI);
 
-    //Motor and servo declaration
-    private DcMotor leftBack, rightBack, leftFront, rightFront; //Initializes direct current main wheel motors for the driving function of our robot, gary.
-    private DcMotor linearSlide;
+    protected DcMotor leftBack, rightBack, leftFront, rightFront; //Initializes direct current main wheel motors for the driving function of our robot, gary.
+    protected DcMotor linearSlide;
     //private Servo hLinearSlide;
-    private Servo clawServo;
+    protected Servo vArmServo, hArmOpen, hLinearSlide, hClawServo;
+
+
+    protected int transferStep;
+    protected boolean enableTransfer;
+    protected ElapsedTime transferTimer = new ElapsedTime();
 
     @Override
     public void runOpMode() {
@@ -34,22 +37,28 @@ public class StatesAuto extends LinearOpMode {
 
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         leftFront.setDirection(DcMotor.Direction.REVERSE);
+        linearSlide.setDirection(DcMotor.Direction.FORWARD);
 
-        clawServo = hardwareMap.get(Servo.class, "cs");
+        vArmServo = hardwareMap.get(Servo.class, "vas");
+
+        hArmOpen = hardwareMap.get(Servo.class, "hao");
+        hLinearSlide = hardwareMap.get(Servo.class, "hls");
+        hClawServo = hardwareMap.get(Servo.class, "hcs");
+
         telemetry.addData("Starting pos: ", leftBack.getCurrentPosition());
         telemetry.update();
         waitForStart();
 
     }
-
     protected enum dir { // dir is short for direction btw
         LEFT,
         RIGHT,
         FORWARD,
-        BACKWARD
+        BACKWARD,
+        LEFTROT,
+        RIGHTROT
 
     }
-
     protected void driveInches(float inches, float speed, dir direction, float timeoutS) {
         double lbDir = 1;
         double rbDir = 1;
@@ -87,7 +96,7 @@ public class StatesAuto extends LinearOpMode {
                 rfDir = -1;
                 break;
         }
-         if(opModeIsActive()) {
+        if(opModeIsActive()) {
             runtime.reset();
             lbTargetPos = (int)(inches * ticksPerInch * lbDir);
             rbTargetPos = (int)(inches * ticksPerInch * rbDir);
@@ -99,7 +108,7 @@ public class StatesAuto extends LinearOpMode {
             rightBack.setTargetPosition(rbTargetPos + rightBack.getCurrentPosition());
             leftFront.setTargetPosition(lfTargetPos + leftFront.getCurrentPosition());
             rightFront.setTargetPosition(rfTargetPos + rightFront.getCurrentPosition());
-            
+
             //TODO: tweek tolerance
             //leftBack.setTargetPositionTolerance(3);
             //rightBack.setTargetPositionTolerance(3);
@@ -115,25 +124,61 @@ public class StatesAuto extends LinearOpMode {
             rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            
+
             while(opModeIsActive() && timeoutS < runtime.seconds() && (leftBack.isBusy() && rightBack.isBusy() && leftFront.isBusy() && rightFront.isBusy())) {
                 telemetry.addData("currently going", String.valueOf(direction));
                 telemetry.update();
             }
-         }
-         leftBack.setPower(0);
-         rightBack.setPower(0);
-         leftFront.setPower(0);
-         rightFront.setPower(0);
+        }
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+        leftFront.setPower(0);
+        rightFront.setPower(0);
 
     }
 
 
+    protected void moveClaw(boolean openClaw) {
+        if(openClaw) {
+            hClawServo.setPosition(0.377);
+        } else {
+            hClawServo.setPosition(0.75);
+        }
+    }
 
+    protected void transferSample() {
+        transferStep = 0;
+        enableTransfer = true;
+        transferTimer.reset();
 
-
+        if(enableTransfer) {
+            if(transferStep == 0) {
+                hArmOpen.setPosition(0.15);
+                hLinearSlide.setPosition(0.605);
+                transferTimer.reset();
+                transferStep = 1;
+            } else if(transferStep == 1 && transferTimer.milliseconds() >= 1200) {
+                hClawServo.setPosition(0.375);
+                transferTimer.reset();
+                transferStep = 2;
+            } else if(transferStep == 2 && transferTimer.milliseconds() >= 500) {
+                hLinearSlide.setPosition(0.575);
+                transferTimer.reset();
+                transferStep = 3;
+            } else if(transferStep == 3 && transferTimer.milliseconds() >= 200) {
+                hClawServo.setPosition(0.75);
+                transferTimer.reset();
+                transferStep = 4;
+            } else if(transferStep == 4 && transferTimer.milliseconds() >= 100) {
+                transferStep = 0;
+                transferTimer.reset();
+                enableTransfer = false;
+            }
+        }
+    }
 
 }
+
 
 
 
